987ad353d89a6a841bb56d4d835cbba1d6324094
Joseph C Wang <joequant@gmail.com>
2022-02-06 14:37:44 +0800

    add general build.sh

--- js/build-flock-body.sh
+++ js/build-flock-body.sh
@@ -12,7 +12,20 @@ cp -rf $script_dir/*.sh \
    $script_dir/*.ts \
    $script_dir/*.json \
    $script_dir/.*.json $mountpoint/opt/$name
-buildah run $container /opt/$name/build.sh $name
+
+buildah config --workingdir /opt/$name $container
+npm install
+
+if [ -f $script_dir/build.sh ] ; then
+    buildah run $container /opt/$name/build.sh $name
+fi
+
+buildah run $container sh -c 'rm /etc/dnf/protected.d/*'
+buildah run $container npm prune --production
+buildah run $container npm cache clean --force
+buildah run $container dnf autoremove -y \
+	tar gcc make git npm shadow-utils sudo vim-minimal
+buildah run $container dnf clean all
 
 # Entrypoint, too, is a “buildah config” command
 buildah config --cmd /opt/$name/run.sh $container
