025559cffb35131c74b7b227fb337b3138962dba
Joseph C Wang <joequant@gmail.com>
2022-02-06 12:19:10 +0800

    add build-flock-body.sh

---
+++ js/build-flock-body.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+set -o errexit
+
+# Create a container
+container=$(buildah from $base)
+mountpoint=$(buildah mount $container)
+# Labels are part of the "buildah config" command
+buildah config --label maintainer="$maintainer" $container
+
+mkdir -p $mountpoint/opt/$name
+cp -r $script_dir/*.sh \
+   $script_dir/*.ts \
+   $script_dir/*.json \
+   $script_dir/.*.json \
+   $script_dir/test $mountpoint/opt/$name
+buildah run $container /opt/$name/build.sh $name
+
+# Entrypoint, too, is a “buildah config” command
+buildah config --cmd /opt/$name/run.sh $container
+
+# Finally saves the running container to an image
+buildah commit --squash --format docker $container $name:latest
