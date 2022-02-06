f925c52732bcd02dc453ca3512e5fafe1eb8ce1d
Joseph C Wang <joequant@gmail.com>
2022-02-06 19:23:06 +0800

    add functions to flock-lib.ts

--- js/flock-lib.ts
+++ js/flock-lib.ts
@@ -6,3 +6,12 @@ export function mySplit (
   const parts = string.split(delimiter)
   return parts.slice(0, n - 1).concat([parts.slice(n - 1).join(delimiter)])
 }
+
+export function isObject (a: any) {
+  return (!!a) && (a.constructor === Object)
+}
+
+export function getErrorMessage (error: unknown) {
+  if (error instanceof Error) return error.message
+  return String(error)
+}
