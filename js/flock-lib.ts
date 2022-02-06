27c83361f16eb1566a8eef9dfc57047f58dd7b88
Joseph C Wang <joequant@gmail.com>
2022-02-06 22:21:43 +0800

    add more tsdoc comments

--- js/flock-lib.ts
+++ js/flock-lib.ts
@@ -1,3 +1,17 @@
+/**
+ * Miscellanous utility functions
+ *
+ * @module
+ */
+
+/**
+ * Solit string into maximum of n strings
+ *
+ * @param string - string to split
+ * @param delimter - delimiter of split
+ * @param n - max strings
+ */
+
 export function mySplit (
   string: string,
   delimiter: string,
@@ -7,9 +21,21 @@ export function mySplit (
   return parts.slice(0, n - 1).concat([parts.slice(n - 1).join(delimiter)])
 }
 
+/**
+ * Check if is object
+ * 
+ * @param a - object to be checked
+ */
+
 export function isObject (a: any) {
   return (!!a) && (a.constructor === Object)
 }
+
+/**
+ * Convert error message to string
+ *
+ * @param error - error object to convert to string
+ */
 
 export function getErrorMessage (error: unknown) {
   if (error instanceof Error) return error.message
